4115d9daf335306af77752b4d5cff66330ee7f17
ritickkumar <ritickkumar08@gmail.com>
2026-01-29 21:27:32 +0530

    added a get request with id

--- index.js
+++ index.js
@@ -18,6 +18,18 @@ app.get('/users', (req,res) =>{
     res.status(200).json(users)
 })
 
+// 2.GET /users/:id – Fetch details of a specific user by ID.
+app.get('/users/:id', (req,res)=>{
+    const userInfo = users.find((user) => user.id == req.params.id) //here we try finding the user that has the same id as searched in the URL.
+
+    if(!userInfo){ //if the user doesn't exists we return with a negative response
+        return res.status(404).json({message: "user doesn't exists in the System"})
+    }
+
+    res.status(200).json(userInfo)// if the user exists we return its information.
+})
+
+
 
 app.listen(PORT, ()=>{
     console.log(`server listening on the port ${PORT}`);
